a1ad6dcc872142006f54fa8d8372becb82adcc08
mjosipovic4821 <mjosipovic4821it@raf.rs>
2025-12-20 18:46:54 +0100

    Added desc to each blank page

--- resources/views/nabavka/index.blade.php
+++ resources/views/nabavka/index.blade.php
@@ -1,7 +1,6 @@
-{{--
-    @extends('layouts.app')
+@extends('layouts.app')
 
-    @section('content')
-        nabavka.index template
-    @endsection
---}}
+@section('content')
+    <h1>{{ ucfirst($resource ?? 'Stranica') }}</h1>
+    <p>CRUD stranica generisana Blueprint alatom.</p>
+@endsection
