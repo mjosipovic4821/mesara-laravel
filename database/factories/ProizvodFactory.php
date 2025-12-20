ac92fc37c586a211e234fcd0e4ba22e556a494cc
mjosipovic4821 <mjosipovic4821it@raf.rs>
2025-12-20 15:55:37 +0100

    Fix ProizvodFactory unique generation

--- database/factories/ProizvodFactory.php
+++ database/factories/ProizvodFactory.php
@@ -11,12 +11,26 @@ class ProizvodFactory extends Factory
      */
     public function definition(): array
     {
+        $nazivi = [
+            'Juneće mleveno meso',
+            'Svinjski vrat',
+            'Pileći file',
+            'Kobasica domaća',
+            'Ćevapi sveži',
+            'Pljeskavica',
+            'Slanina dimljena',
+            'Suva pečenica',
+            'Junetina za supu',
+            'Svinjska rebra',
+        ];
+        $suffix = $this->faker->unique()->numberBetween(1, 10000);
+
         return [
-            'naziv_proizvoda' => fake()->regexify('[A-Za-z0-9]{120}'),
-            'tip' => fake()->regexify('[A-Za-z0-9]{30}'),
-            'prodajna_cena' => fake()->randomFloat(2, 0, 9999999999.99),
-            'zaliha' => fake()->randomFloat(3, 0, 999999999.999),
-            'aktivan' => fake()->boolean(),
+            'naziv_proizvoda' => $this->faker->randomElement($nazivi) . " #{$suffix}",
+            'tip' => $this->faker->randomElement(['gotov', 'sirovina_prodajna']),
+            'prodajna_cena' => $this->faker->randomFloat(2, 300, 3500),
+            'zaliha' => $this->faker->randomFloat(3, 0, 200),
+            'aktivan' => $this->faker->boolean(90),
         ];
     }
 }
